5421d76ab3435b4e5b5cf3e5e8c290f5736703d9
profjguilherme-fatec <jose.pauleti@fatec.sp.gov.br>
2025-08-09 10:54:08 -0300

    Versão 1.0 Robô

---
+++ monstros_de_bytes.java
@@ -0,0 +1,58 @@
+package aprendizado;
+import robocode.*;
+import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * PrimeiroRobo - a robot by (your name here)
+ */
+public class PrimeiroRobo extends Robot
+{
+	/**
+	 * run: PrimeiroRobo's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		setColors(Color.green,Color.green,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(500);
+			back(100);
+			turnLeft(45);
+			turnRight(180);
+			
+						
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		
+	}	
+}
