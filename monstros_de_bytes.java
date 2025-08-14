683b3c1b4a891189809177f4645bbed56503a43a
profjguilherme-fatec <jose.pauleti@fatec.sp.gov.br>
2025-08-14 15:14:24 -0300

    Webhook 5 - teste

--- monstros_de_bytes.java
+++ monstros_de_bytes.java
@@ -24,7 +24,7 @@ public class PrimeiroRobo extends Robot
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			ahead(1000);
+			ahead(1);
 			back(10000);
 			turnLeft(10000);
 			turnRight(100000);
